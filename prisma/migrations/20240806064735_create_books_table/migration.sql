f6b3fa161477b341e228dcb24fed1019020d6638
irwantowibowotech <irwantowibowotech@gmail.com>
2024-08-06 14:06:23 +0700

    feat: integrate app with prisma and postgresql

---
+++ prisma/migrations/20240806064735_create_books_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "books" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("id")
+);
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [BooksController],
   providers: [BooksService]
 })
